de47ba1d0921b8613900d19181222d59f3e41eb7
RyzovaTech <ryzovatech@gmail.com>
2026-09-03 17:42:27 +0530

    Remove tracked build artifacts and dependencies

--- dist/assets/separator-DiMDu78q.js
+++
@@ -1,6 +0,0 @@
-import{c as v,j as s,P as m,l as f}from"./index-DGi6-5rm.js";import{r as l}from"./vendor-react-g8jU58oI.js";/**
- * @license lucide-react v0.453.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const E=v("Download",[["path",{d:"M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4",key:"ih7n3h"}],["polyline",{points:"7 10 12 15 17 10",key:"2ggqvy"}],["line",{x1:"12",x2:"12",y1:"15",y2:"3",key:"1vk2je"}]]);var h="Separator",n="horizontal",x=["horizontal","vertical"],p=l.forwardRef((a,r)=>{const{decorative:t,orientation:o=n,...i}=a,e=y(o)?o:n,d=t?{role:"none"}:{"aria-orientation":e==="vertical"?e:void 0,role:"separator"};return s.jsx(m.div,{"data-orientation":e,...d,...i,ref:r})});p.displayName=h;function y(a){return x.includes(a)}var c=p;const u=l.forwardRef(({className:a,orientation:r="horizontal",decorative:t=!0,...o},i)=>s.jsx(c,{ref:i,decorative:t,orientation:r,className:f("shrink-0 bg-border",r==="horizontal"?"h-[1px] w-full":"h-full w-[1px]",a),...o}));u.displayName=c.displayName;export{E as D,u as S};
